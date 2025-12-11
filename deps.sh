a2951732946289f587b4d16847882436eba00b4d
Ryan Awad <ryanawad@yahoo.com>
2025-12-11 17:14:37 -0400

    added deps

---
+++ deps.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+pacman -Syu \
+	waybar \
+	hyprlock \
+	hyprpaper \
+	rofi \
+	kitty \
+	ttf-font-awesome \
+	nwg-displays
+
+pacman -Syu \
+	btop \
+	neovim \
+	htop \
+	nvtop
+
+pacman -Syu \
+	yazi \
+	zellij \
+	fastfetch \
+	firefox
+
+# INSTALL CASCADIACOVE NERD FONT .....
+
