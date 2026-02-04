2192be5956220f5a41cd4fc82a0f6aca0d3ee02e
Ryan Awad <ryanawad@yahoo.com>
2026-02-04 07:26:57 -0500

    got cava in the waybar

--- waybar/scripts/playerinfo.sh
+++ waybar/scripts/playerinfo.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-playerctl metadata --format '{{artist}} — {{title}}'
+playerctl metadata --format '{{artist}} — {{title}}' | awk '{if (length($0) > 60) print substr($0, 1, 57) "..."; else print}'
