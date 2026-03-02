7e7e0d0ab73febc77733cb37ef35bd720fff9119
Ryan Awad <ryanawad@yahoo.com>
2026-03-02 12:49:58 -0500

    tweaked media player

--- waybar/scripts/playerinfo.sh
+++ waybar/scripts/playerinfo.sh
@@ -2,10 +2,18 @@
 
 artist=$(playerctl metadata artist)
 
+metadata=$(playerctl metadata --format '{{duration(position)}} / {{duration(mpris:length)}} ♫')
 if [ -z "$artist" ]; then
-    metadata=$(playerctl metadata --format '{{title}}')
+    metadata="$metadata $(playerctl metadata --format '{{title}}')"
 else
-    metadata=$(playerctl metadata --format '{{artist}} — {{title}}')
+    metadata="$metadata $(playerctl metadata --format '{{artist}} — {{title}}')"
+fi
+
+status=$(playerctl status)
+if [[ $status == "Playing" ]]; then
+    metadata="▶ $metadata"
+elif [[ $status == "Paused" ]]; then
+    metadata="⏸ $metadata"
 fi
 
 echo $metadata | awk '{if (length($0) > 45) print substr($0, 1, 42) "..."; else print}'
